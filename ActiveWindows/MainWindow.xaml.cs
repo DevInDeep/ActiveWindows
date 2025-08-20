08a3298affc299bd5fd7e001a8b0a8e84f9d8f93
vpavlevski <vpavlevski@yahoo.com>
2025-08-20 20:29:15 +0200

    Add project files.

---
+++ ActiveWindows/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ActiveWindows.Win32;
+using System.Windows;
+
+namespace ActiveWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void DisplayOpenWindows(object sender, RoutedEventArgs e)
+        {
+            lstOpenWindows.Items.Clear();
+            foreach (KeyValuePair<IntPtr, string> window in OpenWindowGetter.GetOpenWindows())
+            {
+                IntPtr handle = window.Key;
+                string title = window.Value;
+
+                lstOpenWindows.Items.Add($"{handle}: {title}");
+            }
+        }
+    }
+}
