efdc8cd1dd256efeefd62da08f85efc88b3ba1f1
vpavlevski <vpavlevski@yahoo.com>
2025-08-23 13:31:46 +0200

    Add WindowsApi

---
+++ ActiveWindows/Win32/WindowsApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveWindows.Win32
+{
+    internal static class WindowsApi
+    {
+    }
+}
