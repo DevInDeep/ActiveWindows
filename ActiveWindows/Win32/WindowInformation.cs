598dc6c6d78f16847c0c426e0cb698caf924371d
vpavlevski <vpavlevski@yahoo.com>
2025-08-24 08:32:07 +0200

    Remove unused code

---
+++ ActiveWindows/Win32/WindowInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using ActiveWindows.Common.Extensions;
+
+namespace ActiveWindows.Win32
+{
+    public class WindowInformation
+    {
+        public string Caption { get; set; } = string.Empty;
+        public string ClassName { get; set; } = string.Empty;
+        public Process Process { get; set; } = null;
+        public Rect WindowRect { get; set; }
+        public bool IsWindowsUI => ClassName.Contains("Windows.UI");
+        public bool IsOsWindow => Process.ProcessIsStartedFromWindowsDirectory() || IsWindowsUI;
+    }
+}
